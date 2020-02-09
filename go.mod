72eae6211ecca92c4a1c1badbb99acf41d423638
Shiraaz Moollatjie <mllshi011@gmail.com>
2020-02-09 22:29:26 +0200

    godeleteempty: First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ShiraazMoollatjie/godeleteempty
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var rootDir = flag.String("rootDir", ".", "The root directory to start from")
+var dryRun = flag.Bool("dryRun", true, "A dryrun prints out all the file moves instead of actually moving them.")
+
+func deleteEmptyDirs(path string, info os.FileInfo, err error) error {
+	if !info.IsDir() || path == *rootDir {
+		return nil
+	}
+
+	d, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+
+	f, err := d.Readdir(0)
+	if err != nil {
+		return err
+	}
+
+	if len(f) == 0 {
+		if *dryRun {
+			log.Printf("%s", path)
+		} else {
+			err := os.Remove(path)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	flag.Parse()
+
+	// TODO(shiraaz): This should be a depth first search for empty directories.
+	filepath.Walk(*rootDir, deleteEmptyDirs)
+}
